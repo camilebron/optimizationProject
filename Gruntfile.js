1c763694e688339a0f6a04aaf7cd870649b8c4f7
Camille Lebron <camilebron@gmail.com>
2016-12-13 14:40:03 -0400

    gruntfile updated with css min

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,24 +9,37 @@ grunt.initConfig({
       // The source file
       src: 'index.html',
       // The destination file
-      dest: 'index.html'
+      dest: 'dest/index.html'
       }
     },
   uglify: {
     my_target: {
       files: {
-        'dest/output.min.js': ['src/input1.js', 'src/input2.js']
+        'dest/js/output.min.js': ['js/*.js']
       }
     }
+  },
+  cssmin: {
+  target: {
+    files: [{
+      expand: false,
+      cwd: 'release/css',
+      src: ['*.css', '!*.min.css'],
+      dest: 'dest/css',
+      ext: '.min.css'
+    }]
   }
+}
   });
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-critical');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default tasks.
-  grunt.registerTask('default', ['critical']);
+  grunt.registerTask('default', ['critical', 'uglify', 'cssmin']);
+
 
 };
 
